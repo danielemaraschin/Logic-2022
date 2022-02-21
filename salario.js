57f2b2b0abea4ade0d7eba8103b3e18e3a9d846b
danielemaraschin <danikmaraschin@gmail.com>
2022-02-21 10:34:43 +0100

    starting with simple functions

--- salario.js
+++ salario.js
@@ -1,11 +1,22 @@
 var salarioMes = 5000;
 var horasTrabalhadasSemana = 36
-var valorHora = 0;
+
 
 function horasTrabalhadas() {
     var horasMes = horasTrabalhadasSemana * 4;
     return horasMes;
     
 }
+ 
 
-console.log(horasTrabalhadas());
+function valorHora() {
+   var horasMes= horasTrabalhadas()
+   // var valorHora = 0
+   
+    var valorHora = salarioMes/horasMes
+    return valorHora
+
+}
+
+
+console.log(valorHora())
