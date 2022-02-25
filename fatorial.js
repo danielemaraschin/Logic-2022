722d5ef2aece4b20c7859e3a95c83148d5b9f6e2
danielemaraschin <danikmaraschin@gmail.com>
2022-02-25 09:40:07 +0100

    working with for

--- fatorial.js
+++ fatorial.js
@@ -1,9 +1,10 @@
 var aleatorio = 5
 var numeroMultiplica = aleatorio - 1
+var resultado = 0
 
 for( var i = numeroMultiplica ; i> 0; i--)    
     while (numeroMultiplica > 0){
-    var resultado = aleatorio * numeroMultiplica
+    resultado = aleatorio * numeroMultiplica
 }
 
-console.log(resultado)
+console.log("testando")
