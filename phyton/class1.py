60226b172b322ff4ce0abe0c772f0db6c6be2b0c
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 05:45:27 +0100

    Learning a little about python

--- phyton/class1.py
+++ phyton/class1.py
@@ -1,2 +1,13 @@
 var velocidadeInternet = 1
 console.log(velocidadeInternet)
+
+nota_filme = 8.5 #float
+#valores booleanos
+aberto = True
+# True e False deve começar c letra masculina
+#python é sensitivo, precisa
+
+print(aberto)
+
+#strings
+console.log("text")
