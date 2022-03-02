7376c6d3425120167714d51386fa3f4255fd6c9a
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 06:24:36 +0100

    working with if, elif and else

---
+++ phyton/elif.py
@@ -0,0 +1,12 @@
+'''
+esta atrasado e pode entrar na aula se for a terceira vez será suspenso, se for menos que isso ai pode entrar sim 
+
+
+elif else
+'''
+
+numero_de_atrasos = 2;
+if numero_de_atrasos >= 3: print ('Você está suspenso')
+elif numero_de_atrasos == 1: print('pode entrar, mas se tomar mais 2 atrasos será suspenso.')
+elif numero_de_atrasos == 2 : print ('pode entrar, mas se tomar mais 3 atrasos, será suspenso.')
+else: print ('pode entrar')
