3cab01e9af32d2b53fba506a5fbbdee5f4ea3e01
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 05:48:19 +0100

    Learning a little about psedocode

---
+++ phyton/class2.py
@@ -0,0 +1,13 @@
+''''
+comentarios em python entre 3x '
+
+
+PSEUDOCÓDIGO
+
+input salario_mensal
+input horas_trabalhadas_mes
+valor_hora = salario mensal/ horas_trabalhadas_mes
+print valor_hora
+'''
+
+slkjldkj
