a77fe975a065dd7fd3fc43af79e879682ed75dea
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 06:05:43 +0100

    Learning a little about psedocode

--- phyton/class2.py
+++ phyton/class2.py
@@ -6,8 +6,10 @@ PSEUDOCÓDIGO
 
 input salario_mensal
 input horas_trabalhadas_mes
-valor_hora = salario mensal/ horas_trabalhadas_mes
+valor_hora = salario_mensal/ horas_trabalhadas_mes
 print valor_hora
 '''
 
-slkjldkj
+salario_mensal= input('qual é o seu salario mensal?')
+horas_trabalhadas_mes = input('quantas horas vc trabalha ao mes?')
+valor_hora = int(salario_mensal)/ int(horas_trabalhadas_mes)
