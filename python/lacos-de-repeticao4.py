fe194fde7b443806cca5895b58f5339fb0ee090d
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 17:01:24 +0100

    working with for and python

---
+++ python/lacos-de-repeticao4.py
@@ -0,0 +1,15 @@
+#imprime os valores de 1 a N
+
+''' INPUT numero MAX
+valor inicial = 1
+loop de valor_inicial a numero_maximo
+passar para numero, pq recebe as var como string
+print Valor
+
+'''
+
+valor_maximo = int(input ("Digite o valor máximo"))
+valor_inicial = int(1)
+
+for numero in range (valor_inicial, valor_maximo + 1) : 
+  print (numero)
