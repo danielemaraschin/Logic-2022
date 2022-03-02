d0618cd581caf5097f05ecd0c94932f6c15f0ddb
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 16:16:44 +0100

    working with if and else

---
+++ python/ifelse.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite o 1 valor: ')
+segundo_valor = input('Digite o 2 valor: ')
+
+if int(primeiro_valor) > int(segundo_valor):
+    print('O primeiro valor é maior.')
+else: 
+  print('O segundo valor é maior.')
