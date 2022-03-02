6ee00f019b88ade6db56f9acbb2896ed737a25dc
danielemaraschin <danikmaraschin@gmail.com>
2022-03-02 16:18:09 +0100

    working with if and else

--- python/ifelse.py
+++ python/ifelse.py
@@ -5,3 +5,5 @@ if int(primeiro_valor) > int(segundo_valor):
     print('O primeiro valor é maior.')
 else: 
   print('O segundo valor é maior.')
+
+#nao consegue comparar duas string por isso coloca int na frente das var que serao comparadas
