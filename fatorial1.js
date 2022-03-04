84a755b4b354a02015799eae4870c94aa690a444
danielemaraschin <danikmaraschin@gmail.com>
2022-03-04 05:21:48 +0100

    fatorial

---
+++ fatorial1.js
@@ -0,0 +1,11 @@
+var numeroInput = 10
+var inicial = 1
+var fatorial = 0
+var multiplicacao = 0
+
+for( var i = 0; i< numeroInput; i++){
+    multiplicacao =  inicial * numeroInput
+     fatorial = fatorial + (multiplicacao)
+}
+
+console.log(fatorial)
